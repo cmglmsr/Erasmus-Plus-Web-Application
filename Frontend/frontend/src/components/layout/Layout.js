42dfa3c4379d34dfaae544ea2be20aa314cf59f0
aslikaraman <asli.karaman92@gmail.com>
2022-11-20 17:03:08 +0300

    table

--- Frontend/frontend/src/components/layout/Layout.js
+++ Frontend/frontend/src/components/layout/Layout.js
@@ -1,8 +1,10 @@
 import NavigationBar from './NavigationBar';
+import Table from './Table';
 function Layout() {
   return (
     <div>
       <NavigationBar></NavigationBar>
+      <Table></Table>
       <main className=""></main>
     </div>
   );
