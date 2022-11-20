d6cf3ae7bd0bc7687de1fd614b0173b386b00936
elfosena <elifsenao@gmail.com>
2022-11-21 00:03:00 +0300

    ROUTERLI NAVBAR

--- Frontend/frontend/src/components/layout/NavigationBar.js
+++ Frontend/frontend/src/components/layout/NavigationBar.js
@@ -1,36 +1,38 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
-import classes from './NavigationBar.module.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 
 function NavigationBar() {
   return (
     <header>
-
       <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">Erasmus Bilkent</Navbar.Brand>
-        <Navbar.Text>
-            Student
-          </Navbar.Text>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#link">Profile</Nav.Link>
-          </Nav>
-          <Form inline >
-              <Button variant="danger" href="/logout">Logout</Button>
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            Erasmus Bilkent
+          </Navbar.Brand>
+          <Navbar.Text>Student</Navbar.Text>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="#link">
+                Profile
+              </Nav.Link>
+            </Nav>
+            <Form inline>
+              <Button variant="danger" as={Link} to="/logout">
+                Logout
+              </Button>
             </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
-    
   );
 }
 export default NavigationBar;
