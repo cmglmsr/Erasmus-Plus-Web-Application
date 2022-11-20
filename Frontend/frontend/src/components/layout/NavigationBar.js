374085b5505dd2df6130d75c9ea2b677385e8702
elfosena <elifsenao@gmail.com>
2022-11-20 16:44:57 +0300

    meov

--- Frontend/frontend/src/components/layout/NavigationBar.js
+++ Frontend/frontend/src/components/layout/NavigationBar.js
@@ -2,22 +2,21 @@ import { Link } from "react-router-dom";
 
 function NavigationBar() {
   return (
-    <header className="">
-      <div className="">React Meetups</div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">All Meetups</Link>
-          </li>
-          <li>
-            <Link to="/">Add New Meetups</Link>
-          </li>
-          <li>
-            <Link to="/">My Favorites</Link>
-          </li>
-        </ul>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Erasmus Bilkent</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="/">Home<span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Features</a>
+            <a class="nav-item nav-link" href="#">Pricing</a>
+            <a class="nav-item nav-link disabled" href="#">Disabled</a>
+          </div>
+        </div>
       </nav>
-          
     </header>
   );
 }
