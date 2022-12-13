d232e63d236e9a557e707b65b4a5e2fcd71ebea2
Eren Duran <erenduran02@gmail.com>
2022-12-13 22:54:12 +0300

    lombok application

--- Backend/src/main/java/com/crackware/erasmus/data/message/ResponseApplication.java
+++ Backend/src/main/java/com/crackware/erasmus/data/message/ResponseApplication.java
@@ -1,5 +1,10 @@
 package com.crackware.erasmus.data.message;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ResponseApplication {
     private String fullname;
     private String id;
@@ -12,46 +17,6 @@ public class ResponseApplication {
         this.id = id;
         this.cgpa = cgpa;
         this.school = school;
-        this.status = status;
-    }
-
-    public String getFullname() {
-        return fullname;
-    }
-
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getCgpa() {
-        return cgpa;
-    }
-
-    public void setCgpa(String cgpa) {
-        this.cgpa = cgpa;
-    }
-
-    public String getSchool() {
-        return school;
-    }
-
-    public void setSchool(String school) {
-        this.school = school;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
         this.status = status;
     }
 }
