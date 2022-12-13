fd88b57c7b578f502b24a1b9479194aa15841821
Arda YILDIZ <ardayildiz1074@gmail.com>
2022-12-13 22:18:21 +0300

    ISO Controller

--- Backend/src/main/java/com/crackware/erasmus/web/controller/InternationalStudentOfficeController.java
+++ Backend/src/main/java/com/crackware/erasmus/web/controller/InternationalStudentOfficeController.java
@@ -5,20 +5,25 @@ import com.crackware.erasmus.data.model.Student;
 import com.crackware.erasmus.data.services.InternationalStudentOfficeService;
 import com.crackware.erasmus.data.services.StudentService;
 import com.crackware.erasmus.data.services.helper.HelperService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.crackware.erasmus.data.services.impl.FileUploadServiceImpl;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping({"iso", "/iso"})
 public class InternationalStudentOfficeController {
 
     private final HelperService helperService;
+    private final FileUploadServiceImpl fileUploadService;
 
     private final InternationalStudentOfficeService isoService;
-    public InternationalStudentOfficeController(HelperService helperService, StudentService studentService, InternationalStudentOfficeService isoService) {
+    public InternationalStudentOfficeController(HelperService helperService, StudentService studentService,
+                                                InternationalStudentOfficeService isoService, FileUploadServiceImpl fileUploadService) {
         this.helperService = helperService;
         this.isoService = isoService;
+        this.fileUploadService = fileUploadService;
     }
 
     @GetMapping("/home")
@@ -30,14 +35,10 @@ public class InternationalStudentOfficeController {
         return (InternationalStudentOffice) helperService.getUser();
     }
 
-    @RestController
-    @CrossOrigin("http://localhost:8080") // Change cross-origin when hosted
-    public class FileUploadController {
-        private final FileUploadServiceImpl fileUploadService;
     @PostMapping("/home")
-    public void uploadFile(@RequestParam("transcript") MultipartFile file) throws IOException {
+    public void uploadFile(@RequestParam("transcript") MultipartFile transcript) throws IOException {
         try {
-            fileUploadService.store(file);
+            fileUploadService.store(transcript);
             System.out.println("[+] Transcript upload successful.");
         } catch (IOException e) {
             System.out.println("[-] Transcript upload failed.");
