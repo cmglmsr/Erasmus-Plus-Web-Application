caac7b11ce59cd0779c18821db00c98fba088f6d
elfosena <elifsenao@gmail.com>
2022-12-13 17:52:03 +0300

    REDİRECT

--- Frontend/frontend/src/pages/LoginPage.js
+++ Frontend/frontend/src/pages/LoginPage.js
@@ -4,16 +4,23 @@ import Row from "react-bootstrap/Row";
 
 function LoginPage() {
   function onLoginHandler(loginData) {
-      console.log(loginData);
+    console.log(loginData);
     fetch(
       "http://localhost:8080/api/auth/signin", //enter api address
       {
         method: "POST",
-        body: JSON.stringify(loginData), headers: {
-              "Content-Type": "application/json",
-          },
+        body: JSON.stringify(loginData),
+        headers: {
+          "Content-Type": "application/json",
+        },
       }
-    );
+    ).then((response) => {
+      if (response.roles === "ROLE_STUDENT") {
+        window.location.href = "http://localhost:8080/"
+      }
+      // HTTP 301 response
+      // HOW CAN I FOLLOW THE HTTP REDIRECT RESPONSE?
+    });
   }
   return (
     <section>
