730db0680c324f6f7fce55a85a1ea4aa9c0333a2
Eren Duran <erenduran02@gmail.com>
2022-12-15 18:36:38 +0300

    ToDoList and Schedule added with controllers

---
+++ Backend/src/main/java/com/crackware/erasmus/data/model/Schedule.java
@@ -0,0 +1,31 @@
+package com.crackware.erasmus.data.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Setter
+@Getter
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String date;
+
+    private String dueDate;
+
+    private String description;
+
+    private boolean done;
+
+
+
+}
