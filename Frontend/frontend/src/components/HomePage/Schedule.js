af99edd26c51f2d32e1932eefcac8e934070c33e
cmglmsr <mgulumser@my.aci.k12.tr>
2022-12-18 02:30:46 +0300

    Schedule Updated

--- Frontend/frontend/src/components/HomePage/Schedule.js
+++ Frontend/frontend/src/components/HomePage/Schedule.js
@@ -18,54 +18,46 @@ function Schedule() {
         <tr>
           <th>#</th>
           <th>Due date</th>
-          <th>Due time</th>
           <th>Task</th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <td>1</td>
-          <td></td>
-          <td></td>
-          <td></td>
-        
+          <td>01.11.2022</td>
+          <td>Erasmus Applications Open for Students</td>
+
         </tr>
         <tr>
           <td>2</td>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td>01.01.2023</td>
+          <td>Erasmus Applications Close for Students</td>
         </tr>
         <tr>
           <td>3</td>
-          <td></td>
-          <td></td>
-          <td></td>
-      
+          <td>15.01.2023</td>
+          <td>Erasmus Application Results Are Announced</td>
+
         </tr>
         <tr>
           <td>4</td>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td>01.02.2023</td>
+          <td>Erasmus Applications Finalized</td>
         </tr>
         <tr>
           <td>5</td>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td>20.02.2023</td>
+          <td>Course Wishlist Creation Deadline for Students</td>
         </tr>
         <tr>
           <td>6</td>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td>15.03.2023</td>
+          <td>PreApproval Submit Deadline for Students</td>
         </tr>
         <tr>
           <td>7</td>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td>01.04.2023</td>
+          <td>Learning Agreement Submit Deadline for Students</td>
         </tr>
         </tbody>
         </Table>
