c6c85c1e3860f81002c7fe647580d5990435bc99
cmglmsr <mgulumser@my.aci.k12.tr>
2022-12-18 04:48:10 +0300

    18.12.2022 Final Version

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -40,8 +40,7 @@ import LearningAgreementListPage from "./pages/LearningAgreementListPage";
 import LearningAgreementListContextLayout from "./context/LearningAgreementContext/LearningAgreementListContextLayout";
 import PreApprovalListContextLayout from "./context/PreApprovalContext/PreApprovalListContextLayout";
 import PreApprovalListPage from "./pages/Fbm/PreApprovalListPage";
-import TranscriptListPage from "./pages/TranscriptListPage";
-import TranscriptListContext from "./context/TranscriptContext/TranscriptListContextLayout";
+
 
 const DUMMY_PROFILE = {
   role: "Student",
@@ -218,14 +217,6 @@ function App() {
             exact={true}
             element={<IsoProfile />}
           ></Route>
-
-          <Route element={<TranscriptListContext />}>
-            <Route
-              path="/iso/transcript"
-              exact={true}
-              element={<TranscriptListPage />}
-            ></Route>
-          </Route>
         </Route>
 
         <Route
